3f3f851798882c48d8d2c3a96ab8919ff6f80d65
socx <socx9@yahoo.com>
2020-04-03 10:09:31 +0100

    test database connection

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Contacts from './components/contacts';
+import {
+  QA_API_BASE, LOCAL_API_BASE, CONTACTS_ENDPOINT
+} from './constants/endpoints'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const CONTACT_ENDPOINT =  `${QA_API_BASE}${CONTACTS_ENDPOINT}` ||
+  `${LOCAL_API_BASE}${CONTACTS_ENDPOINT}`;
+
+class App extends Component {
+  state = {
+    contacts: []
+  }
+  componentDidMount() {
+    fetch(CONTACT_ENDPOINT)
+    .then(res => res.json())
+    .then((response) => {
+      this.setState({ contacts: response.data })
+    })
+    .catch(console.log)
+  } 
+  render() {
+    return (
+      <div>
+        {this.state.contacts && this.state.contacts.length &&
+          <Contacts contacts={this.state.contacts} />
+        }
+        {(!this.state.contacts || !this.state.contacts.length) &&
+          <p>no contacts</p>
+        }
+      </div>
+      
+    )
+  }
 }
 
 export default App;
+
+
+
+// import React, { Component } from 'react';
+// import Contacts from './components/contacts';
+
+// const CONTACT_ENDPOINT = `https://my-money.herokuapp.com/api/countries` ||
+//   'http://localhost:3008/api/contacts';
+
+// class App extends Component {
+//   state = {
+//     contacts: []
+//   }
+//   componentDidMount() {
+//     fetch(CONTACT_ENDPOINT)
+//     .then(res => res.json())
+//     .then((response) => {
+//       this.setState({ contacts: response.data })
+//     })
+//     .catch(console.log)
+//   } 
+//   render() {
+//     return (
+//       <div>
+//         {this.state.contacts && this.state.contacts.length &&
+//           <Contacts contacts={this.state.contacts} />
+//         }
+//         {!this.state.contacts || !}
+//       </div>
+      
+//     )
+//   }
+// }
+
+// export default App;
---
+++ src/constants/endpoints.js
@@ -0,0 +1,3 @@
+export const QA_API_BASE = 'https://socx-money-api.herokuapp.com/api'
+export const LOCAL_API_BASE = 'http://localhost:3008/api'
+export const CONTACTS_ENDPOINT = '/contacts';
